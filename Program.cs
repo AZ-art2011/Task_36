1813e2ef5165f1119becf49f21a5e1a73cf79aca
Alexander <mypost@example.com>
2023-01-15 16:32:45 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях. 
+// task 36
+
+Console.Clear();
+int val = 0;
+int[] RandArray = new int[14];
+
+for (int i=0; i<RandArray.Length; i++)
+{
+    RandArray[i] = new Random().Next(-99, 100);
+    if (i%2 != 0) val = val + RandArray[i];
+}
+
+Console.Write($"Сумма элементов массива ");
+Console.Write($" [ {String.Join(", ", RandArray)}]");
+Console.Write($", стоящих на нечетных позициях: {val}.");
